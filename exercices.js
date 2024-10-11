28a7953df860b8c08cbfd72731d0f303d9baf365
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2024-10-11 16:01:34 +0100

    solution d'exercice 5

--- exercices.js
+++ exercices.js
@@ -2,6 +2,7 @@
 // Déclare une variable prenom et attribue-lui ton prénom. Ensuite, affiche cette variable dans la console.
 const prenom = "ayoub";
 console.log(prenom);
+console.log("****************************");
 
 // Exercice 2 : Manipulation de variables numériques
 // Déclare deux variables a et b avec des valeurs numériques.
@@ -13,6 +14,7 @@ console.log(`la somme de ${a} et ${b}: ${a + b}`);
 console.log(`la différence entre ${a} et ${b}: ${a - b}`);
 console.log(`le produit de ${a} * ${b}: ${a * b}`);
 console.log(`le quotient de ${a} / ${b}: ${a / b}`);
+console.log("****************************");
 
 // Exercice 3 : Concaténation de chaînes de caractères
 // Déclare deux variables nom et prenom. Crée une nouvelle variable nomComplet qui combine ces deux variables avec un espace entre elles.
@@ -21,6 +23,7 @@ const nom = "AKRAOU",
 	prenom_ = "Ayoub";
 const nomComplet = nom + " " + prenom_;
 console.log(nomComplet);
+console.log("****************************");
 
 // Exercice 4 : Utilisation des variables booléennes
 // Déclare une variable age et attribue-lui une valeur numérique.
@@ -29,3 +32,17 @@ console.log(nomComplet);
 const age = 23;
 const majeur = age >= 18 ? true : false;
 console.log(majeur);
+console.log("****************************");
+
+// Exercice 5 : Échange de valeurs entre deux variables
+// Déclare deux variables x et y avec des valeurs différentes.
+// Échange les valeurs de x et y sans déclarer de nouvelles variables.
+// Affiche les nouvelles valeurs de x et y dans la console.
+let x = 1,
+	y = 0;
+console.log("avant l'échange:");
+console.log("x: " + x, "y: " + y);
+[x, y] = [y, x];
+console.log("apres l'échange:");
+console.log("x: " + x, "y: " + y);
+console.log("****************************");
