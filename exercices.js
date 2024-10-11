b405110cdf54c03e075f2eeb38189146ccd31e77
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2024-10-11 15:32:06 +0100

    solution d'exercice 2

--- exercices.js
+++ exercices.js
@@ -2,3 +2,14 @@
 // Déclare une variable prenom et attribue-lui ton prénom. Ensuite, affiche cette variable dans la console.
 const prenom = "ayoub";
 console.log(prenom);
+
+// Exercice 2 : Manipulation de variables numériques
+// Déclare deux variables a et b avec des valeurs numériques.
+// Affiche la somme, la différence, le produit et le quotient de ces deux nombres dans la console.
+const a = 20,
+	b = 5;
+
+console.log(`la somme de ${a} et ${b}: ${a + b}`);
+console.log(`la différence entre ${a} et ${b}: ${a - b}`);
+console.log(`le produit de ${a} * ${b}: ${a * b}`);
+console.log(`le quotient de ${a} / ${b}: ${a / b}`);
