9baca6a8d4d4fcd15fde42e8a340c9e21ea4c107
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2024-10-11 15:24:30 +0100

    solution d'exercice 1

---
+++ exercices.js
@@ -0,0 +1,4 @@
+// Exercice 1 : Déclaration et affectation de variables
+// Déclare une variable prenom et attribue-lui ton prénom. Ensuite, affiche cette variable dans la console.
+const prenom = "ayoub";
+console.log(prenom);
